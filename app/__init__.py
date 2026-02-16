f043c9019f21b68a591f898f16f6318dbad001cd
Oksana <oxemelyanovamx5@gmail.com>
2026-02-16 10:47:07 +0100

    structure change, separation of main and reading feature, usage of blueprints for both

--- app/__init__.py
+++ app/__init__.py
@@ -4,3 +4,28 @@
 # Registers blueprints
 
 # So it builds the main application
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from .config import DevelopmentConfig
+
+db = SQLAlchemy()
+
+def create_app(config_class=DevelopmentConfig):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+
+    db.init_app(app)
+
+    #Register blueprint for reading which I haven't coded yet
+    from .reading import reading_bp
+    # url prefix is for the browser, it will add "reading" infront of any reading url
+    app.register_blueprint(reading_bp, url_prefix="/reading")
+
+    # need to handle the main route here either view function or also
+    # a blueprint for main
+    # decided that I will for cleaningness
+    from .main import main_bp
+    app.register_blueprint(main_bp, url_prefix="/main")
+
+    return app
